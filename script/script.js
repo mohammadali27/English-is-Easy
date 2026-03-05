83002e5ac235aa371fe7df4de982695f397ad764
mohammadali27 <amohammad6627@gmail.com>
2026-03-05 22:46:25 +0600

    dialog add

--- script/script.js
+++ script/script.js
@@ -3,12 +3,52 @@ const lodeData = () => {
     .then((res) => res.json())
     .then((json) => displeyLesons(json.data));
 };
+const removeActiveClass =()=>{
+  const allBtns=document.querySelectorAll(".all-Btn");
+  allBtns.forEach(btn => btn.classList.remove("btn-active"));
+  
+}
 const lodeLavelData =(id)=>{
   
   const url =`https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
   .then(res => res.json())
-  .then((data) => displeylevelwoeds(data.data));
+  .then((data) =>{
+    removeActiveClass();
+    const allBtns=document.querySelectorAll(".all-Btn");
+    const lessonbbtn= document.getElementById(`lesson-btn-${id}`);
+    // document.querySelectorAll(`lesson-btn-${id}`)
+    // .forEach(btn => btn.classList.remove("btn-active"));
+    //  lessonbbtn.classList.add("btn-active");
+
+    // lessonbbtn.classList.remove("btn-active");
+    lessonbbtn.classList.add("btn-active");
+     displeylevelwoeds(data.data)
+  });
+}
+const lodeWorld = async (id)=>{
+  const url=`https://openapi.programming-hero.com/api/word/${id}`;
+  console.log(url);
+  
+  const res = await fetch(url); 
+  const detail = await res.json();
+  displeyWordDetails(detail.data);
+};
+const displeyWordDetails = (word) =>{
+console.log(word);
+const ditilesContener = document.getElementById("ditiles-contener");
+ditilesContener.innerHTML = `
+ <div class="flex flex-col  space-y-5">
+ ${word.word ? `<h3 class="text-lg font-bold">Word:<img class=" inline-block" src="./assets/Group 9.png" alt="">( ${word.word})</h3>` : ''}
+ ${word.pronunciation ? `<h3 class="text-lg font-bold">Pronunciation: ${word.pronunciation}</h3>` : ''}
+ ${word.meaning ? `<p class="py-4 font-bold">Meaning: ${word.meaning}</p>` : ''}
+ ${word.partsOfSpeech ? `<p class="py-4 font-bold">Parts of Speech: ${word.partsOfSpeech}</p>` : ''}
+ ${word.sentence ? `<p class="py-4 font-bold">Sentence: ${word.sentence}</p>` : ''}
+ ${word.synonym ? `<p class="py-4">Synonym: ${word.synonym}</p>` : ''}
+ </div>`;
+document.getElementById("my_modal_5").showModal();
+
+
 }
 const displeylevelwoeds =(words)=>{
 const wordContainer = document.getElementById("word-container");
@@ -25,7 +65,7 @@ if (words.length==0) {
   
 }
 words.forEach(word => {
-  console.log(word);
+  
 //    {
 //     "id": 5,
 //     "level": 1,
@@ -36,12 +76,12 @@ words.forEach(word => {
   
  const card = document.createElement("div");
  card.innerHTML = `
- <div class="bg-white rounded-xl shadow-sm text-center px-5 py-10 space-y-5">
+ <div class=" bg-white rounded-xl shadow-sm text-center px-5 py-10 space-y-5">
         <h2>${word.word ? word.word : "Word not available"}</h2>
         <p>Meaning /Pronounciation</p>
         <div>${word.meaning ? word.meaning : "Sorry, meaning not available"} / ${word.pronunciation ? word.pronunciation : "Sorry, pronunciation not available"}</div>
         <div class=" flex justify-between items-center mt-5">
-         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+         <button onclick="lodeWorld(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
         
@@ -59,7 +99,7 @@ displeyLesons = (lesons) => {
     console.log(lesion);
 
     const btnDiv = document.createElement("div");
-    btnDiv.innerHTML = `<button onclick="lodeLavelData(${lesion.level_no})" class=" btn btn-outline btn-primary mb-4">
+    btnDiv.innerHTML = `<button id="lesson-btn-${lesion.level_no}" onclick="lodeLavelData(${lesion.level_no})" class="all-Btn btn btn-outline btn-primary mb-4">
                       <i class="fa-solid fa-book-open">
                       </i>Lesson - ${lesion.level_no}</button>`;
     levelContainer.appendChild(btnDiv);
